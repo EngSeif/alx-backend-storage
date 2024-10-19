105d0006018b9fa04d341c6d84f34ddfab2d3942
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-19 15:37:34 +0300

    Adv MySql | Procedure 2 fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,15 +5,29 @@ DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(5, 2);
+    DECLARE correction_count INT DEFAULT 0;
+    DECLARE project_sum FLOAT DEFAULT 0;
 
-    SELECT AVG(score) INTO avg_score
+    -- Count the number of corrections for the given user
+    SELECT COUNT(*) INTO correction_count
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = input_user_id;
 
-    UPDATE users 
-    SET average_score = IFNULL(avg_score, 0)
-    WHERE id = input_user_id;
+    -- Sum the scores from corrections for the given user
+    SELECT SUM(score) INTO project_sum
+    FROM corrections 
+    WHERE user_id = input_user_id;
+
+    -- Calculate and update the average score in the users table
+    IF correction_count > 0 THEN
+        UPDATE users 
+        SET average_score = project_sum / correction_count
+        WHERE id = input_user_id;
+    ELSE
+        UPDATE users 
+        SET average_score = 0
+        WHERE id = input_user_id;
+    END IF;
 
 END $$
 
