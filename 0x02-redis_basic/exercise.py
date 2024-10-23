bb389358b12c14755c22271f4616dba451c70851
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-23 20:18:44 +0300

    Redis | Cache Class fix 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-*Redis basic
+Redis basic
 """
 
 import redis
@@ -10,24 +10,24 @@ from typing import Union
 
 class Cache:
     """
-    *Cache Class To Store Cache Data
-    *into Redis Server
+    Cache Class To Store Cache Data
+    into Redis Server
     """
     def __init__(self) -> None:
         """
-        *Initialize Redis client and flush the database.
+        Initialize Redis client and flush the database.
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, float, int, bytes]) -> str:
         """
-        *Store the data in Redis with a random key and return the key.
-        !Args:
-            *data: The data to store in Redis
-            *(can be str, bytes, int, or float).
-        !Returns:
-            *str: The key under which the data is stored.
+        Store the data in Redis with a random key and return the key.
+        Args:
+            data: The data to store in Redis
+            (can be str, bytes, int, or float).
+        Returns:
+            str: The key under which the data is stored.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
