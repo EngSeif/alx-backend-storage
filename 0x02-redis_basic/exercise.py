5d2e148432e442a0e966ba9646ea1d0aed6c3a1c
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-23 20:10:48 +0300

    Redis | Cache Class Doc

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,10 +14,21 @@ class Cache():
     *into Redis Server
     """
     def __init__(self) -> None:
+        """
+        *Initialize Redis client and flush the database.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, float, int, bytes]) -> str:
+        """
+        *Store the data in Redis with a random key and return the key.
+        !Args:
+            *data: The data to store in Redis
+            *(can be str, bytes, int, or float).
+        !Returns:
+            *str: The key under which the data is stored.
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
